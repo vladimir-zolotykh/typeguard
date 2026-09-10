436b2199674e178be3c37c1c917ef6a14b52444c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-10 10:55:05 +0300

    Implement autoassert

--- autoassert.py
+++ autoassert.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from functools import wraps
+from inspect import signature, _empty
 
 
-def autoassert():
-    pass
+def autoassert(func):
+    sig = signature(func)
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        bound = sig.bind(*args, **kwargs)
+        for name, parm in sig.parameters.items():
+            if parm.annotation is not _empty:
+                assert isinstance(
+                    bound.arguments[name], parm.annotation
+                ), f"{name!r}: type mismatch, expected {parm.annotation!r}"
+        res = func(*args, **kwargs)
+        return res
+
+    return wrapper
 
 
 class Person:
+    @autoassert
     def __init__(self, name: str, age: int, salary: float):
         for k, v in locals().items():
             if k == "self":
